5d8c6f80ff31030823d556351d84fe5877aee517
LuisAlvarado-19 <luafer95@gmail.com>
2021-03-01 20:59:07 +0000

    nuevo repositorio

---
+++ src/js/component/Cards.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+export function Cards() {
+	return (
+		<div className="d-flex justify-content-around">
+			<div className="card mx-3" style={{ width: "18rem" }}>
+				<img
+					src="http://placehold.it/500x325.jpg"
+					className="card-img-top"
+					alt="..."
+					height="266.23px"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Some quick example text to build on the card tittle and
+						make up the bulk of the cards content.
+					</p>
+					<a
+						href="https://es.wikipedia.org/wiki/Kimi_no_Na_wa."
+						className="btn btn-info">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+
+			<div className="card mx-3" style={{ width: "18rem" }}>
+				<img
+					src="http://placehold.it/500x325.jpg"
+					className="card-img-top"
+					alt="..."
+					height="266.23px"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Some quick example text to build on the card tittle and
+						make up the bulk of the cards content.
+					</p>
+					<a
+						href="https://es.wikipedia.org/wiki/El_gran_showman"
+						className="btn btn-info">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div className="card mx-3" style={{ width: "18rem" }}>
+				<img
+					src="http://placehold.it/500x325.jpg"
+					className="card-img-top"
+					alt="..."
+					height="266.23px"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Some quick example text to build on the card tittle and
+						make up the bulk of the cards content.
+					</p>
+					<a
+						href="https://es.wikipedia.org/wiki/Gastronom%C3%ADa_de_M%C3%A9xico"
+						className="btn btn-info">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+
+			<div className="card mx-3" style={{ width: "18rem" }}>
+				<img
+					src="http://placehold.it/500x325.jpg"
+					className="card-img-top"
+					alt="..."
+					height="266.23px"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Some quick example text to build on the card tittle and
+						make up the bulk of the cards content.
+					</p>
+					<a
+						href="https://flordecana.com/es/collection.html"
+						className="btn btn-info">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/js/component/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<nav
+			className="navbar navbar-dark bg-dark"
+			style={{ marginTop: "2rem" }}>
+			<a className="mx-auto" style={{ color: "white" }}>
+				Copyright © Your Webside 2021
+			</a>
+		</nav>
+	);
+}
